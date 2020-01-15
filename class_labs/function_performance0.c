5e41c3484e92a9767be25e4383ffc6fd67205bdb
STUPID-AF-CAT <geddy@waveville.net>
2020-01-15 13:38:41 -0600

    Update function_performance0.c

--- class_labs/function_performance0.c
+++ class_labs/function_performance0.c
@@ -1,17 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 
 int replace_character(char * the_string, const char find_me, const char replace_me);
 
 int main(void)
 {
-    char the_string[60] = "Uh nu, I seem tu have misplaced my o's.\n";
+    char the_string[256] = {0};
     char find_me;
     char replace_me;
+    printf("Type something lol: ");
+    fgets(the_string, 255, stdin);
     printf("What letter do you want to replace? ");
-    find_me = getchar();
+    scanf("\n%c", &find_me);
     printf("With what letter? ");
-    replace_me = getchar();
+    scanf(" %c", &replace_me);
     printf("You got it baws.\n");
 
 
@@ -30,6 +33,11 @@ int replace_character(char * the_string, const char find_me, const char replace_
         if (the_string[x] == find_me)
         {
             the_string[x] = replace_me;
+            replaced++;
+        }
+        else if (toupper(the_string[x]) == toupper(find_me))
+        {
+            the_string[x] = toupper(replace_me);
             replaced++;
         }
         x++;
